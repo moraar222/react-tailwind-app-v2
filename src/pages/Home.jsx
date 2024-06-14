8f32672f838dfbf22ed03e57903a7d0ab7a35225
Moraar222 <moraar976@gmail.com>
2024-06-14 23:38:30 +0300

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to Home Page</h1>
+      <h1 className='text-red-200 font-bold bg-pink-700'>Welcome to Home Page</h1>
     </div>
   );
 };
